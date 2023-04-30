90f4a348f467a5660a03ccf0ad227682c896f223
razvan700 <110129667+razvan700@users.noreply.github.com>
2023-04-30 21:53:02 +0300

    Added printing state method that occurs for every stage of guessing

--- Hangman/src/Hangman.java
+++ Hangman/src/Hangman.java
@@ -9,22 +9,27 @@ public class Hangman {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("D:\\JavaApps\\Hangman\\Words_alpha.txt"));
         List<String> wordsList = new ArrayList<>();
-        while (scanner.hasNext()){
+        while (scanner.hasNext()) {
             wordsList.add(scanner.nextLine());
         }
 
-        Random randomizer =new Random();
+        Random randomizer = new Random();
         String word = wordsList.get(randomizer.nextInt(wordsList.size()));
         System.out.println(word);
         List<Character> currentGuess = new ArrayList<>();
 
-        for(int i=0;i< word.length();i++){                  //print current state for each guess
-            if(currentGuess.contains(word.charAt(i)))
+        printCurrentState(word, currentGuess);
+
+        printCurrentState(word, currentGuess);
+    }
+    private static void printCurrentState (String word, List < Character > currentGuess){
+        for (int i = 0; i < word.length(); i++) {
+            if (currentGuess.contains(word.charAt(i))) {
                 System.out.print(word.charAt(i));
-            else {
+            } else {
                 System.out.print("-");
             }
         }
-
+        System.out.println();
     }
 }
